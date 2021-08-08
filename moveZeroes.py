5fbdbafc535a3d8a1803c7b70497c8c72ab25fef
fon40 <fon400@163.com>
2021-08-08 21:21:05 +0800

    Add files via upload

---
+++ moveZeroes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return nums
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+=1
+        return nums
